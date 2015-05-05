c3155747d90c7f355acb455353f3df9ebbd9d31d
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2015-05-05 18:36:25 -0300

    added log

--- mongo-install.sh
+++ mongo-install.sh
@@ -1,4 +1,9 @@
+echo "adding key"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "adding source"
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+echo "updating and intalling latest mongodb"
 sudo apt-get update
 sudo apt-get install -y mongodb-org
