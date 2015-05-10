14d2bb40e1e17d63aea639357167739737a723d5
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2015-05-10 15:58:50 -0300

    updated filesystem commands

--- FileSystem/src/consola/consola.c
+++ FileSystem/src/consola/consola.c
@@ -7,13 +7,15 @@
 void readCommand(char *command, int maximoLargo);
 void freeSplits(char ** splits);
 void formatMDFS();
+
+void deleteResource(char **parameters);
 void deleteFile(char *file);
-void renameFile(char *file);
-void moveFile(char *file);
-void createDir(char *dir);
 void deleteDir(char *dir);
-void renameDir(char *dir);
-void moveDir(char *dir);
+
+void moveResource(char *resource);
+
+void createDir(char *dir);
+
 void copyToMDFS(char *file);
 void copyToFS(char *file);
 void MD5(char *file);
@@ -25,119 +27,50 @@ void deleteNode(char *node);
 void help();
 int isNullParameter(char *parameter);
 
-//Incializo los comandos en un array para despues poder hacer
-//el switch y llamar a la funcion correspondiente
-const char* const validCommands[]= {
-				"format",
-				"deleteFile",
-				"renameFile",
-				"moveFile",
-				"createDir",
-				"deleteDir",
-				"renameDir",
-				"moveDir",
-				"copyToMDFS",
-				"copyToFS",
-				"MD5",
-				"seeBlock",
-				"deleteBlock",
-				"copyBlock",
-				"upNode",
-				"deleteNode",
-				"help",
-				"exitMDFS"	//Aunque no tenga una funcion, lo asigno asi no aparece comando invalido cuando salgo
-};
-
 void startConsole() {
-	// // lo defino como un char** porque necesito tener un "array"
-	//con todas las cadenas, donde la primera es el comando y las demas los parametros
-	char **splitCommand;
+	// lo defino como un char** porque necesito tener un "array" de los parametros
+	char **parameters;
+	//char *command;
 	char command[100];
-	int exit;
-	int i;
+	int exit = false;
 
 	do {
-		printf(">");
-		int option = 18; //18 es la opcion de comando Invalido
+		printf("> ");
 		readCommand(command, 100);
-		printf("\n");
 
-		//Cuando recibo el comando lo separo en comando-parametros (1 por ahora)
-		splitCommand = string_split(command, " ");
+		parameters = string_split(command, " ");
 
-		//Una vez que lo separe reviso si la primera parte de los splits splitCommand[0]
-		//se corresponde con alguno de los comandos validos para asignar la opcion
-		for (i = 0; i < 18; i++) {
-			if (string_equals_ignore_case(splitCommand[0], validCommands[i])) {
-				option = i;
-			}
-		}
-
-		switch (option) {
-		//Uso splitCommand[1] porque es la parte de los parametros cuando agreguemos mas parametros
-		//a las funciones deberiamos revisar cuantos parametros le mandamos y mandarlos asi:
-		//por ejemplo para 3 (splitCommand[1],splitCommand[3],splitCommand[3])
-		case 0:
+		if (string_equals_ignore_case(parameters[0], "format")) {
 			formatMDFS();
-			break;
-		case 1:
-			deleteFile(splitCommand[1]);
-			break;
-		case 2:
-			renameFile(splitCommand[1]);
-			break;
-		case 3:
-			moveFile(splitCommand[1]);
-			break;
-		case 4:
-			createDir(splitCommand[1]);
-			break;
-		case 5:
-			deleteDir(splitCommand[1]);
-			break;
-		case 6:
-			renameDir(splitCommand[1]);
-			break;
-		case 7:
-			moveDir(splitCommand[1]);
-			break;
-		case 8:
-			copyToMDFS(splitCommand[1]);
-			break;
-		case 9:
-			copyToFS(splitCommand[1]);
-			break;
-		case 10:
-			MD5(splitCommand[1]);
-			break;
-		case 11:
-			seeBlock(splitCommand[1]);
-			break;
-		case 12:
-			deleteBlock(splitCommand[1]);
-			break;
-		case 13:
-			copyBlock(splitCommand[1]);
-			break;
-		case 14:
-			upNode(splitCommand[1]);
-			break;
-		case 15:
-			deleteNode(splitCommand[1]);
-			break;
-		case 16:
+		} else if (string_equals_ignore_case(parameters[0], "rm")) {
+			deleteResource(parameters);
+		} else if (string_equals_ignore_case(parameters[0], "mv")) {
+			moveResource(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "mkdir")) {
+			createDir(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "md5")) {
+			MD5(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "copyToMDFS")) {
+			copyToMDFS(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "copyToFS")) {
+			copyToFS(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "seeBlock")) {
+			seeBlock(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "deleteBlock")) {
+			deleteBlock(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "copyBlock")) {
+			copyBlock(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "upNode")) {
+			upNode(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "deleteNode")) {
+			deleteNode(parameters[1]);
+		} else if (string_equals_ignore_case(parameters[0], "help")) {
 			help();
-			break;
-		case 18:
-			printf("Comando Invalido\n");
-			break;
-
+		} else if (string_equals_ignore_case(parameters[0], "exit")) {
+			exit = true;
 		}
-	exit = string_equals_ignore_case(splitCommand[0], "exitMDFS");
 
-	//string_n_splits supongo que va haciendo malloc o algo parecido para ir asignando las cadenas,
-	//para evitar que halla perdidas de memoria, esta funcion va liberando uno por uno los splits en splitcommand
-	freeSplits(splitCommand);
+		freeSplits(parameters);
 	} while (!exit);
 
 }
@@ -146,7 +79,7 @@ void readCommand(char *lectura, int maximoLargo) {
 	//Habria que buscar una funcion parecida a fgets, pero que no sea necesario un maximo, asi evitamos poner un maximo arbitrario
 	fgets(lectura, maximoLargo, stdin);
 
-	if ((strlen(lectura) > 0) && (lectura[strlen(lectura) - 1] == '\n')) {
+	if ((strlen(lectura) > 1) && (lectura[strlen(lectura) - 1] == '\n')) {
 		//agrego el /0 para que lo considere como una cadena
 		lectura[strlen(lectura) - 1] = '\0';
 	}
@@ -180,94 +113,99 @@ void formatMDFS() {
 	printf("Formatea el MDFS\n");
 }
 
+void deleteResource(char **parameters) {
+	if (string_equals_ignore_case(parameters[1], "-r")) {
+		deleteDir(parameters[2]);
+	} else {
+		deleteFile(parameters[1]);
+	}
+}
+
 void deleteFile(char *file) {
-	if (!isNullParameter(file))
+	if (!isNullParameter(file)) {
 		printf("Borra el archivo %s\n", file);
-}
-
-void renameFile(char *file) {
-	if (!isNullParameter(file))
-		printf("Renombra el archivo %s\n", file);
-}
-
-void moveFile(char *file) {
-	if (!isNullParameter(file))
-		printf("Mueve el archivo %s\n", file);
-}
-
-void createDir(char *dir) {
-	if (!isNullParameter(dir))
-		printf("Crea el directorio %s\n", dir);
+	}
 }
 
 void deleteDir(char *dir) {
-	if (!isNullParameter(dir))
+	if (!isNullParameter(dir)) {
 		printf("Borra el directorio %s\n", dir);
+	}
 }
 
-void renameDir(char *dir) {
-	if (!isNullParameter(dir))
-		printf("Renombra el directorio %s\n", dir);
+
+void moveResource(char *resource) {
+	if (!isNullParameter(resource)) {
+		printf("Mueve el recurso %s\n", resource);
+	}
 }
 
-void moveDir(char *dir) {
-	if (!isNullParameter(dir))
-		printf("Mueve el directorio %s\n", dir);
-}
-
-void copyToMDFS(char *file) {
-	if (!isNullParameter(file))
-		printf("Copia el archivo %s al MDFS\n", file);
-}
-
-void copyToFS(char *file) {
-	if (!isNullParameter(file))
-		printf("Copia el archivo %s al FileSystem\n", file);
+void createDir(char *dir) {
+	if (!isNullParameter(dir)) {
+		printf("Crea el directorio %s\n", dir);
+	}
 }
 
 void MD5(char *file) {
-	if (!isNullParameter(file))
+	if (!isNullParameter(file)) {
 		printf("Obtiene el MD5 de %s\n", file);
+	}
 }
 
+void copyToMDFS(char *file) {
+	if (!isNullParameter(file)) {
+		printf("Copia el archivo %s al MDFS\n", file);
+	}
+}
+
+void copyToFS(char *file) {
+	if (!isNullParameter(file)) {
+		printf("Copia el archivo %s al FileSystem\n", file);
+	}
+}
+
+
 void seeBlock(char *block) {
-	if (!isNullParameter(block))
+	if (!isNullParameter(block)) {
 		printf("Vee el Bloque nro %s\n", block);
+	}
 }
 
 void deleteBlock(char *block) {
-	if (!isNullParameter(block))
+	if (!isNullParameter(block)) {
 		printf("Borra el Bloque nro %s\n", block);
+	}
 }
 
 void copyBlock(char *block) {
-	if (!isNullParameter(block))
+	if (!isNullParameter(block)) {
 		printf("Copia el Bloque nro %s\n", block);
+	}
 }
 
 void upNode(char *node) {
-	if (!isNullParameter(node))
+	if (!isNullParameter(node)) {
 		printf("Agrega el nodo %s\n", node);
+	}
 }
 
 void deleteNode(char *node) {
-	if (!isNullParameter(node))
+	if (!isNullParameter(node)) {
 		printf("Borra el nodo %s\n", node);
+	}
 }
 
 void help() {
 	printf("Comandos Validos\n");
 	printf("formatMDFS		Formatea el MDFS\n");
-	printf("deleteFile file		Borra el archivo file\n");
-	printf("renameFile file		Renombra el archivo file\n");
-	printf("moveFile file		Mueve el archivo file\n");
-	printf("createDir dir		Crea un directorio llamado dir\n");
-	printf("deleteDir dir		Borra el directorio dir\n");
-	printf("renameDir dir		Renombra el directorio dir\n");
-	printf("moveDir dir		Mueve el directorio dir\n");
+	printf("rm file		Borra el archivo file\n");
+	printf("rm -r dir		Borra el directorio dir\n");
+	printf("mv file		Mueve el archivo file\n");
+	printf("mv dir		Mueve el directorio dir\n");
+	printf("mkdir dir		Crea un directorio llamado dir\n");
+	printf("MD5 file		Obtiene el MD5 de file\n");
 	printf("copyToMDFS file		Copia el archivo file al MDFS\n");
 	printf("copyToFS file		Copia el archivo file al File System\n");
-	printf("MD5 file		Obtiene el MD5 de file\n");
 	printf("seeBlock block		Muestra el bloque block\n");
 	printf("deleteBlock block	Borra el bloque block\n");
 	printf("copyBlock block		Copia el bloque block\n");
