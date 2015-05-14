fc9147647b7563561d556b59897d967d4734acb8
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2015-05-14 11:20:18 -0300

    added mongo driver install

---
+++ install-mongo-driver.sh
@@ -0,0 +1,6 @@
+wget https://github.com/mongodb/mongo-c-driver/releases/download/1.1.5/mongo-c-driver-1.1.5.tar.gz
+tar xzf mongo-c-driver-1.1.5.tar.gz
+cd mongo-c-driver-1.1.5
+./configure
+make
+sudo make install
